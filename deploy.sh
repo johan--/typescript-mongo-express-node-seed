8251a1b3a1410f36ea40df3eabfbc996c657423b
bersling <bersling@gmail.com>
2017-05-15 02:57:25 +0200

    a bit better layouting

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,29 @@
 server=root@107.170.33.76
 
-# Switch to main folder
 cd ..
 
 # remove the old zip file if present, locally and on server
-rm -f backend.zip
-ssh ${server} "rm -f backend.zip"
+rm -f whats-up-with-angular-universal.zip
+ssh ${server} "rm -f whats-up-with-angular-universal.zip"
 
 # zip the required directories
-zip -r backend.zip backend -x \
-backend/node_modules/**\* \
-backend/dist/**\* >/dev/null
+zip -r whats-up-with-angular-universal.zip whats-up-with-angular-universal -x \
+whats-up-with-angular-universal/node_modules/**\* \
+whats-up-with-angular-universal/dist/**\* >/dev/null
 
 echo ""
 echo "---------------------------------"
 echo "Upload Zip"
 echo "---------------------------------"
 
-scp backend.zip "${server}:~"
+scp whats-up-with-angular-universal.zip "${server}:~"
 
-ssh ${server} "rm -rf backend"
-ssh ${server} "unzip backend.zip"
-ssh ${server} "cd backend && npm install && npm run forever"
+ssh ${server} "rm -rf whats-up-with-angular-universal"
+ssh ${server} "unzip whats-up-with-angular-universal.zip"
+ssh ${server} "cd whats-up-with-angular-universal && npm install && npm run stopforever && npm run forever"
 
 echo ""
 echo "---------------------------------"
 echo "Cleanup"
 echo "---------------------------------"
-rm backend.zip
+rm whats-up-with-angular-universal.zip
